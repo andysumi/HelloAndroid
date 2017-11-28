4bde2b6c0ca46155ef08533e0b59dcc5a00f9d63
Yoshiyuki Takeda <andysumi@ruby.net.tokai-u.jp>
2017-11-28 14:07:17 +0900

    initial commit!!

---
+++ app/src/main/java/com/hoge/helloworld/MainActivity.java
@@ -0,0 +1,41 @@
+package com.hoge.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ボタンを設定
+        Button button = findViewById(R.id.button);
+
+        // TextViewの設定
+        textView = findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // flagがtrueの場合
+                if (flag) {
+                    textView.setText(R.string.hello);
+                    flag = false;
+                }
+                // flagがfalseの場合
+                else {
+                    textView.setText(R.string.world);
+                    flag = true;
+                }
+            }
+        });
+    }
+}
